2106ad7d501347fd7af5963303bc0559009851ee
D4marp <damargalihajipradana@it.student.pens.ac.id>
2025-04-15 16:01:29 +0700

    n

--- 01-writing-comments/index.js
+++ 01-writing-comments/index.js
@@ -1,16 +1,6 @@
-// username: Damar Galih Aji Pradana
+//  Damar Galih Aji Pradana
 /*
 Goal tahun ini:
 1. Belajar JavaScript.
 2. Menjadi Front-End atau Back-End Developer.
-3. Membangun proyek nyata untuk portofolio.
-4. Meningkatkan keterampilan dalam pengembangan perangkat lunak.\
-5. Berkontribusi pada proyek open-source.
-6. Mempelajari framework JavaScript seperti React atau Angular.
-7. Mengikuti kursus online untuk meningkatkan keterampilan.
-8. Membaca buku tentang pengembangan perangkat lunak.
-9. Bergabung dengan komunitas pengembang untuk berbagi pengetahuan.
-10. Membangun jaringan profesional dengan sesama pengembang.
-11. Mempelajari prinsip-prinsip desain perangkat lunak.
-12. Mengikuti konferensi atau seminar tentang teknologi terbaru.
 */
